3c20163e8c52a146af1a0d9545f5570d7068248f
=hmph <=anurag.arnab@gmail.com>
2013-12-07 13:10:54 +0200

    First commit. Should have done this a long time ago

---
+++ Merge/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Merge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.buttonMerge = new System.Windows.Forms.Button();
+            this.buttonDel = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textEnd = new System.Windows.Forms.TextBox();
+            this.textStart = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.browse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.buttonMerge);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonDel);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonDown);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonUp);
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textEnd);
+            this.splitContainer1.Panel2.Controls.Add(this.textStart);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.progressBar1);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.browse);
+            this.splitContainer1.Size = new System.Drawing.Size(723, 390);
+            this.splitContainer1.SplitterDistance = 278;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // buttonMerge
+            // 
+            this.buttonMerge.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonMerge.Location = new System.Drawing.Point(225, 0);
+            this.buttonMerge.Name = "buttonMerge";
+            this.buttonMerge.Size = new System.Drawing.Size(75, 27);
+            this.buttonMerge.TabIndex = 4;
+            this.buttonMerge.Text = "Merge";
+            this.buttonMerge.UseVisualStyleBackColor = true;
+            this.buttonMerge.Click += new System.EventHandler(this.buttonMerge_Click);
+            // 
+            // buttonDel
+            // 
+            this.buttonDel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonDel.Location = new System.Drawing.Point(150, 0);
+            this.buttonDel.Name = "buttonDel";
+            this.buttonDel.Size = new System.Drawing.Size(75, 27);
+            this.buttonDel.TabIndex = 5;
+            this.buttonDel.Text = "Delete";
+            this.buttonDel.UseVisualStyleBackColor = true;
+            this.buttonDel.Click += new System.EventHandler(this.buttonDel_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonDown.Location = new System.Drawing.Point(75, 0);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(75, 27);
+            this.buttonDown.TabIndex = 3;
+            this.buttonDown.Text = "Down";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonUp.Location = new System.Drawing.Point(0, 0);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(75, 27);
+            this.buttonUp.TabIndex = 2;
+            this.buttonUp.Text = "Up";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(723, 251);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textEnd
+            // 
+            this.textEnd.Location = new System.Drawing.Point(664, 49);
+            this.textEnd.Name = "textEnd";
+            this.textEnd.Size = new System.Drawing.Size(56, 20);
+            this.textEnd.TabIndex = 8;
+            this.textEnd.TextChanged += new System.EventHandler(this.textEnd_TextChanged);
+            // 
+            // textStart
+            // 
+            this.textStart.Location = new System.Drawing.Point(576, 49);
+            this.textStart.Name = "textStart";
+            this.textStart.Size = new System.Drawing.Size(60, 20);
+            this.textStart.TabIndex = 7;
+            this.textStart.TextChanged += new System.EventHandler(this.textStart_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(642, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "to";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 26);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Visible = false;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(723, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(645, 82);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(75, 23);
+            this.browse.TabIndex = 0;
+            this.browse.Text = "Browse";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "PDF files |*.pdf|All files|*.*";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 390);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Merge";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonMerge;
+        private System.Windows.Forms.Button buttonDel;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textEnd;
+        private System.Windows.Forms.TextBox textStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Merge/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using org.pdfclown.documents;
+using org.pdfclown.files;
+using org.pdfclown.objects;
+using org.pdfclown.tools;
+
+using PdfSharp;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Drawing;
+
+
+// for outputting to debug window
+
+using System.Diagnostics;
+using System.Collections;
+using org.pdfclown.documents.interaction.viewer;
+using org.pdfclown.documents.interchange.metadata;
+
+namespace Merge
+{
+    public partial class Form1 : Form
+    {
+
+        ArrayList inputFiles = new ArrayList();
+        ArrayList start = new ArrayList();
+        ArrayList end = new ArrayList();
+
+        bool dontUpdate = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+
+            listBox1.DragEnter += new DragEventHandler(DragEnter);
+            listBox1.DragDrop += new DragEventHandler(DragDrop);
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+
+            //         listBox1.DataSource = inputFiles;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Don't want selected item handler to trigger now
+            listBox1.SelectedIndexChanged -= listBox1_SelectedIndexChanged;
+            dontUpdate = true;
+
+            openFileDialog1.ShowDialog();
+
+            String[] files = openFileDialog1.FileNames;
+
+            String list = "";
+
+            foreach (String name in files)
+            {
+                list = list + name + " ";
+                inputFiles.Add(name);
+            }
+
+            refreshList();
+
+            //           textBox1.ReadOnly = false;
+            //           textBox1.Text = list;
+            //           textBox1.ReadOnly = true;
+
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            dontUpdate = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (!dontUpdate)
+            {
+                textBox1.ReadOnly = false;
+                textBox1.Text = "" + listBox1.SelectedIndex;
+                textBox1.ReadOnly = true;
+
+                int startIndex = 0;
+
+                if (listBox1.SelectedIndex >= 0)
+                {
+                    if (((string)inputFiles[listBox1.SelectedIndex]).LastIndexOf("\\") > 0)
+                    {
+                        startIndex = ((string)inputFiles[listBox1.SelectedIndex]).LastIndexOf("\\") + 1;
+                    }
+
+                    using (File mainFile = new File((string) inputFiles[listBox1.SelectedIndex]) )
+                    {
+
+                        label1.Text = ((string)inputFiles[listBox1.SelectedIndex]).Substring(startIndex) + "("+mainFile.Document.Pages.Count+")";
+                    }
+                }
+
+            }
+        }
+
+
+        //Event handlers for drag and drop into the ListBox          
+
+        void DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                if (file.EndsWith(".pdf"))
+                {
+                    inputFiles.Add(file);
+                }
+            }
+            refreshList();
+
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            swap(listBox1.SelectedIndex, listBox1.SelectedIndex - 1, inputFiles);
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            if (swap(listBox1.SelectedIndex, listBox1.SelectedIndex + 1, inputFiles))
+            {
+                listBox1.SelectedIndex += 1;
+            }
+        }
+
+        private bool swap(int index1, int index2, ArrayList array)
+        {
+            if (index1 >= 0 && index2 >= 0 && index1 < array.Count && index2 < array.Count)
+            {
+                Object temp = array[index1];
+                array[index1] = array[index2];
+                array[index2] = temp;
+                refreshList();
+                return true;
+            }
+            return false;
+
+        }
+
+        private void refreshList()
+        {
+            listBox1.DataSource = null;
+            listBox1.DataSource = inputFiles;
+        }
+
+        private void merge(string target)
+        {
+            if (inputFiles.Count > 1)
+            {
+                writeText("Working");
+                progressBar1.Visible = true;
+                progressBar1.Step = 100 / inputFiles.Count;
+                using (File mainFile = new File((string)inputFiles[0]))
+                {
+                    Document doc = mainFile.Document;
+                    Pages pages = doc.Pages;
+
+                    for (int i = 1; i < inputFiles.Count; ++i)
+                    {
+                        using (File sourceFile = new File((string)inputFiles[i]))
+                        {
+                            new PageManager(doc).Add(sourceFile.Document);
+                            progressBar1.PerformStep();
+                        }
+                    }
+
+                    Serialize(mainFile, target, null, "title", "subject", "keywords");
+                }
+            }
+            else
+            {
+                writeWarningText("Require at least two files to merge");
+            }
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void buttonMerge_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            merge(saveFileDialog1.FileName);
+        }
+
+        private string Serialize(File file, string fileName, SerializationModeEnum? serializationMode, string title, string subject, string keywords)
+        {
+            ApplyDocumentSettings(file.Document, title, subject, keywords);
+
+
+            // Standard serialisation
+            serializationMode = (SerializationModeEnum)0;
+
+
+            string outputFilePath = fileName + ".pdf";
+
+            // Save the file!
+            /*
+              NOTE: You can also save to a generic target stream (see Save() method overloads).
+            */
+            try
+            { 
+                file.Save(outputFilePath, serializationMode.Value);
+                writeText("Complete");
+                Process.Start(outputFilePath);
+                progressBar1.Visible = false;
+            }
+            catch (Exception e)
+            {
+
+                writeWarningText("File writing failed");
+                //Console.WriteLine("File writing failed: " + e.Message);
+                //Console.WriteLine(e.StackTrace);
+            }
+            // Console.WriteLine("\nOutput: " + outputFilePath);
+
+            return outputFilePath;
+        }
+
+        private void ApplyDocumentSettings(Document document, string title, string subject, string keywords
+  )
+        {
+            if (title == null)
+                return;
+
+            // Viewer preferences.
+            ViewerPreferences view = new ViewerPreferences(document); // Instantiates viewer preferences inside the document context.
+            document.ViewerPreferences = view; // Assigns the viewer preferences object to the viewer preferences function.
+            view.DisplayDocTitle = true;
+
+            // Document metadata.
+            Information info = document.Information;
+            info.Clear();
+            info.Author = "author";
+            info.CreationDate = DateTime.Now;
+            info.Creator = GetType().FullName;
+            info.Title = title;
+            info.Subject = subject;
+            info.Keywords = keywords;
+        }
+
+        private void buttonDel_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                inputFiles.RemoveAt(listBox1.SelectedIndex);
+                refreshList();
+            }
+        }
+
+        private void writeText(string text)
+        {
+            textBox1.ForeColor = System.Drawing.Color.Black;
+            textBox1.ReadOnly = false;
+            textBox1.Text = text;
+            textBox1.ReadOnly = true;
+        }
+
+        private void writeWarningText(string text)
+        {
+            textBox1.ForeColor = System.Drawing.Color.Crimson;
+            textBox1.ReadOnly = false;
+            textBox1.Text = text;
+            //textBox1.ForeColor = System.Drawing.Color.Black;
+            textBox1.ReadOnly = true;
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void textStart_TextChanged(object sender, EventArgs e)
+        {
+            setRange(textStart, start);            
+        }
+
+        private void setRange(TextBox textBox, ArrayList array)
+        {
+            try
+            {
+                int number = Convert.ToInt32(textBox.Text);
+
+                if (listBox1.SelectedIndex >= 0)
+                {
+                    using (File mainFile = new File((string)inputFiles[listBox1.SelectedIndex]))
+                    {
+                        // pdf numbering starts at 1!
+                        if (number > mainFile.Document.Pages.Count || number < 1)
+                        {
+                            throw new Exception();
+                        }
+                        else
+                        {
+                            array[listBox1.SelectedIndex] = number;
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                writeWarningText("Invalid input");
+            }
+        }
+
+        private void textEnd_TextChanged(object sender, EventArgs e)
+        {
+            setRange(textEnd, end);      
+        }
+        
+    }
+}
+
+
