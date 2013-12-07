8a9907bcaa3e9cf2c9fd1fea87034703c6c2cbee
=hmph <=anurag.arnab@gmail.com>
2013-12-07 13:41:22 +0200

    Using a data type for file now

---
+++ Merge/InputFile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge
+{
+    class InputFile
+    {
+        string path;
+        int start;
+        int end;
+        int length;
+
+        public string Path
+        {
+            set
+            {
+                this.path = value;
+            }
+            get
+            {
+                return this.path;
+            }
+        }
+
+        public int Start
+        {
+            set
+            {
+                this.start = value;
+            }
+            get
+            {
+                return this.start;
+            }
+        }
+
+        public int End
+        {
+            set
+            {
+                this.end = value;
+            }
+            get
+            {
+                return this.end;
+            }
+        }
+
+        public int Length
+        {
+            set
+            {
+                this.length = value;
+            }
+            get
+            {
+                return this.length;
+            }
+        }
+    }
+}
